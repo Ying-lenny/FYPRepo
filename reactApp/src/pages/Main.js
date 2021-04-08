f543da8106ec79c19c6ca9a2e0ecb835bb2dfeb3
Owen Lenihan <owenlenihan101@gmail.com>
2021-04-08 19:43:44 +0100

    Removed Public Page
    Removed a page that wasn't do anything

--- reactApp/src/pages/Main.js
+++ reactApp/src/pages/Main.js
@@ -3,9 +3,9 @@ import { Route, BrowserRouter } from "react-router-dom";
 import Home from "./Home";
 import Stuff from "./Stuff";
 import Contact from "./Contact";
-import { PublicPage, Profile } from "./publicPage";
 import LoginPage from "./loginPage";
 import SignUpPage from "./signUpPage";
+import Profile from "./ProfilePage";
 import PrivateRoute from "./privateRoute";
 import AuthHeader from "../components/authHeader";
 import AuthProvider from "../contexts/authContext";
@@ -23,7 +23,6 @@ class Main extends Component {
               <SiteHeader />
                 <div className="content">
                     <Route exact path="/" component={Home}/>
-                    <Route path="/public" component={PublicPage} />
                     <Route path="/login" component={LoginPage} />
                     <Route path="/signup" component={SignUpPage} />
                     <Route path="/stuff" component={Stuff}/>
